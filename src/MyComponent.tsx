b75d3b4d29103dc0a427223fb6604c51dd917b71
abolhasani2308 <abolhasani2308@gmail.com>
2023-08-24 18:53:58 +0330

    Add sample component

---
+++ src/MyComponent.tsx
@@ -0,0 +1,14 @@
+import {View} from 'react-native';
+
+function MyComponent() {
+  return (
+    <View
+      style={{
+        height: 1000,
+        alignSelf: 'stretch',
+        backgroundColor: 'red',
+      }}></View>
+  );
+}
+
+export default MyComponent;
